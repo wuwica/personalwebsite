3a29b0b7542eafed961f01b2f849d74322d93943
Jonathan Wu <wuwicajon@gmail.com>
2018-04-26 21:54:25 -0400

    change a few of the animations and how they work

--- script.js
+++ script.js
@@ -1,9 +1,30 @@
-var cards = document.getElementsByClassName("card");
+function openTab(evt, tabName) {
+    // Declare all variables
+    var i, tabcontent, tablinks;
 
-var myFunction = function() {
-    document.getElementById("slide-animation")("slide-animate-active");
-};
+    // Get all elements with class="tabcontent" and hide them
+    tabcontent = document.getElementsByClassName("tab");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].classList.add("hidden");
+        tabcontent[i].classList.remove("animate");
+    }
 
-for (var i = 0; i < cards.length; i++) {
-    cards[i].addEventListener('click', myFunction, false);
+    // Get all elements with class="tablinks" and remove the class "active"
+    tablinks = document.getElementsByClassName("card");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+
+    // Show the current tab, and add an "active" class to the button that opened the tab
+   
+    element = document.getElementById(tabName);
+    element.classList.remove("hidden");
+    document.getElementById("slide-animation").classList.remove("slide-animation-play");
+    element.classList.add("animate");
+    
+    void document.getElementById("slide-animation").offsetWidth;
+    
+    document.getElementById("slide-animation").classList.add("slide-animation-play");
+    evt.currentTarget.className += " active";
+    
 }
